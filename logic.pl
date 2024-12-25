cb0c9db9730c9f7ef746e796ec97dae1caa7809c
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2024-12-25 20:00:50 +0000

    added score calculation

--- logic.pl
+++ logic.pl
@@ -52,6 +52,62 @@ display_board(Board):-
 
 
 
+
+
+sum_list([], 0).
+sum_list([H|T], Sum) :-
+    sum_list(T, S),
+    Sum is H + S.
+
+max_list([X], X).
+max_list([H|T], Max) :-
+    max_list(T, M),
+    Max is max(H, M).
+
+cell_player(Board, RowIdx, ColIdx, Player) :-
+    nth1(RowIdx, Board, Row),
+    nth1(ColIdx, Row, Player).
+
+calculate_largest_group(Board, Player, MaxSize) :- 
+    findall(Size, (
+        member(RowIdx, [1,2,3,4,5,6,7,8]), 
+        member(ColIdx, [1,2,3,4,5,6,7,8]), 
+        cell_player(Board, RowIdx, ColIdx, Player), 
+        bfs(Board, Player, [[RowIdx, ColIdx]], [], Size)
+    ), Sizes), 
+    max_list(Sizes, MaxSize).
+
+bfs(_, _, [], _, 0).
+bfs(Board, Player, [[Row, Col] | Queue], Visited, Size) :-
+    \+ member([Row, Col], Visited),
+    cell_player(Board, Row, Col, Player),
+    findall(
+        [R, C], 
+        (neighbor([Row, Col], [R, C]), 
+         R > 0, R =< 8, C > 0, C =< 8, 
+         cell_player(Board, R, C, Player), 
+         \+ member([R, C], Visited)
+        ), 
+        Neighbors
+    ),
+    append(Queue, Neighbors, NewQueue),
+    bfs(Board, Player, NewQueue, [[Row, Col] | Visited], S),
+    Size is S + 1.
+bfs(Board, Player, [_ | Queue], Visited, Size) :-
+    bfs(Board, Player, Queue, Visited, Size).
+
+neighbor([Row, Col], [Row1, Col]) :- Row1 is Row - 1. % Up
+neighbor([Row, Col], [Row1, Col]) :- Row1 is Row + 1. % Down
+neighbor([Row, Col], [Row, Col1]) :- Col1 is Col - 1. % Left
+neighbor([Row, Col], [Row, Col1]) :- Col1 is Col + 1. % Right
+
+calculate_scores(Board, OScore, XScore) :- 
+    calculate_largest_group(Board, o, OScore), 
+    calculate_largest_group(Board, x, XScore).
+
+
+
+
 % Main menu predicate
 main_menu :-
     write('Welcome to Mabula!'), nl,
@@ -66,6 +122,9 @@ handle_choice(1) :-
     write('Starting a new game...'), nl,
     generate_random_board(Board),
     display_board(Board),
+    calculate_scores(Board, OScore, XScore),
+    write('O Score: '), write(OScore), nl,
+    write('X Score: '), write(XScore), nl,
     %...
     main_menu.
 handle_choice(2) :-
